5ea6db50b11b53d3ab41b9c3c8867e7bf28cf89d
amrulhadi90 <78598194+amrulhadi90@users.noreply.github.com>
2021-02-08 08:04:39 +0000

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   global.DATABASE._data.chats[m.chat].isBanned = false
-  m.reply('Done!')
+  m.reply('Done mengaktifkan kembali fitur menu!')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
